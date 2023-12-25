ff017672d19e7d171e17f41f0a5391a6756a516f
Sahil Tamboli <tambolisahil36@gmail.com>
2023-12-25 16:57:46 +0530

    Paper ui Component Added

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { PaperProvider } from 'react-native-paper';
+import { Button } from 'react-native-paper';
 
 export default function App() {
   return (
+    <PaperProvider>
     <View style={styles.container}>
       <Text className="text-red-600">Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button icon="camera" mode="contained" onPress={() => console.log('Pressed')}>
+    Press me
+  </Button>
     </View>
+    </PaperProvider>
   );
 }
 
